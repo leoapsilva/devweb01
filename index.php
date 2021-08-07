7ea88704471b6c9474af797078dd6432f58ef894
Leonardo Alves de Paula e Silva <leoapsilva@gmail.com>
2021-08-06 21:39:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+    $texto = "Hello World, Leo Rici!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> <?php echo $texto; ?> </h1>
+    <form action="/action_page.php">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="John"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="Doe"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
